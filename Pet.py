1448bd297c4dba7040cd005a21ea23db08b7c3af
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 04:49:59 +0800

    Pseudocode

---
+++ Pet.py
@@ -0,0 +1,8 @@
+# create a class pet
+# create constructor with data attribute of name , animal type, and age
+# create an attribute for set name
+# create an attribute for animal type 
+# create an attribute for age
+# return name
+# return animal type
+# return age
