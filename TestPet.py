453904470528e0d4630813327acb29323372728c
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 05:13:40 +0800

    doing testing. done creating set line of codes for creating theobject and calling its attribute

---
+++ TestPet.py
@@ -0,0 +1,8 @@
+from Pet import Pet
+# create an object named Pet
+my_pet = Pet()
+# make the user input all the data attributes needed
+my_pet.set_name()
+my_pet.set_animal_type()
+my_pet.set_age()
+# return all the data attributes and print the output
