1dc57524a3a6bc4e75efd8f67b65bd69e67af7e9
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 05:19:01 +0800

    done retrieving and displaying the output

--- TestPet.py
+++ TestPet.py
@@ -6,3 +6,4 @@ my_pet.set_name()
 my_pet.set_animal_type()
 my_pet.set_age()
 # return all the data attributes and print the output
+print("\nPet's Name: ", my_pet.get_name(), "\nAnimal Type: ", my_pet.get_animal_type(), "\nPet's Age: ", my_pet.get_age())
