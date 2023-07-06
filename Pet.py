6f59dbbdba7e252f72662739e14cea66ba491be8
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 05:08:14 +0800

    done for the attributes of age then returning the attribute values

--- Pet.py
+++ Pet.py
@@ -12,6 +12,8 @@ class Pet:
     def set_animal_type(self):
         self.__animal_type = str(input("My pet is a: "))
 # create an attribute for age
+    def set_age(self):
+        self.__age = str(input("My pets' age is: "))
 # return name
     def get_name(self):
         return self.__name
@@ -19,3 +21,5 @@ class Pet:
     def get_animal_type(self):
         return self.__animal_type
 # return age
+    def get_age(self):
+        return self.__age
