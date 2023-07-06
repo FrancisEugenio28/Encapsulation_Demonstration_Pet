df10b96718a6892b7b9f17b36bd2053588632ff8
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 05:00:03 +0800

    Done for the name input, then do some editing in constructors

--- Pet.py
+++ Pet.py
@@ -1,13 +1,17 @@
 # create a class pet
 class Pet:
 # create constructor with data attribute of name , animal type, and age
-    def __init__(self, name, animal_type, age):
+    def __init__(self):
         self.__name = ''
         self.__animal_type = ''
         self.__age = 0
 # create an attribute for set name
+    def set_name(self):
+        self.__name = str(input("What is the name of your pet?: "))
 # create an attribute for animal type 
 # create an attribute for age
 # return name
+    def get_name(self):
+        return self.__name
 # return animal type
 # return age
