7845d44365e2887e648423e05d511bb0987d7e6f
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 04:55:03 +0800

    done for class and constructor

--- Pet.py
+++ Pet.py
@@ -1,5 +1,10 @@
 # create a class pet
+class Pet:
 # create constructor with data attribute of name , animal type, and age
+    def __init__(self, name, animal_type, age):
+        self.__name = ''
+        self.__animal_type = ''
+        self.__age = 0
 # create an attribute for set name
 # create an attribute for animal type 
 # create an attribute for age
