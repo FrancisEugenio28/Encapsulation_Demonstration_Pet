589350142b05cf2285ec6d927c3153a00ca2cf96
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-07-07 05:02:31 +0800

    done adding attributes for animal type and returning its data

--- Pet.py
+++ Pet.py
@@ -9,9 +9,13 @@ class Pet:
     def set_name(self):
         self.__name = str(input("What is the name of your pet?: "))
 # create an attribute for animal type 
+    def set_animal_type(self):
+        self.__animal_type = str(input("My pet is a: "))
 # create an attribute for age
 # return name
     def get_name(self):
         return self.__name
 # return animal type
+    def get_animal_type(self):
+        return self.__animal_type
 # return age
